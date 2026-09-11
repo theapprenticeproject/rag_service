5d0fcf6c0d6d840c39ac09fe620914c3a2d67864
Manu <manu.a@tap>
2026-09-11 15:28:22 +0530

    model chagned to gemini

--- rag_service/feedback_utils/text_evaluation.py
+++ rag_service/feedback_utils/text_evaluation.py
@@ -17,7 +17,7 @@ class TextEvaluationGenerator(EvaluationGenerator):
         try:
             print("\n=== Starting AI Feedback Generation (Text) ===")
 
-            llm_provider, model_used = self._create_llm_provider("Anthropic")
+            llm_provider, model_used = self._create_llm_provider("Gemini")
             activity_type = assignment_context["assignment"].get("activity_type")
             course_vertical = assignment_context["assignment"].get("course_vertical")
             course_vertical = "Arts"
